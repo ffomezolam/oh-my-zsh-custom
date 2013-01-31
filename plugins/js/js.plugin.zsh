ea7018b1f18c30b3dfe94b42dc8ae3bfc2de61e3
ffomezolam <ffomezolam@gmail.com>
2013-01-30 23:21:27 -0500

    Fixed print formatting

--- plugins/js/js.plugin.zsh
+++ plugins/js/js.plugin.zsh
@@ -10,11 +10,14 @@ alias npmgu='sudo npm -g update'
 newjsumd () {
     [[ -z $EDITOR ]] && EDITOR=vim
     [[ -e $1 ]] && print "$1 exists.  Not modifying.\n" && $EDITOR $1
-    [[ ! -e $1 ]] && print  '/**'"\n"' * @module'"\n"' */'"\n"\
-                            '(function(name, context, definition) {'\
-                            "\t""if(typeof module !== 'undefined' && module.exports) module.exports = definition(require());"\
-                            "\t""else if(typeof define === 'function' && define.amd) define([], definition);"\
-                            "\t""else context[name] = definition();"\
-                            "})('griddy', this, function(H) {"\
-                            "});" > $1 && $EDITOR $1
+    [[ ! -e $1 ]] && print "/**"\
+"\n * @module"\
+"\n */"\
+"\n(function(name, context, definition) {"\
+"\n    if(typeof module !== 'undefined' && module.exports) module.exports = definition(/*require(deps)*/);"\
+"\n    else if(typeof define === 'function' && define.amd) define(/*[deps], */definition);"\
+"\n    else context[name] = definition();"\
+"\n})(/*'name'*/, this, function(/*deps*/) {"\
+"\n"\
+"\n});" > $1 && $EDITOR $1
 }

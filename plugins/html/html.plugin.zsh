acb4e9d416a0433e5b8124c922d537384c77426b
ffomezolam <ffomezolam@gmail.com>
2013-02-07 00:02:37 -0500

    added newphp, which is basically the same as newhtml

--- plugins/html/html.plugin.zsh
+++ plugins/html/html.plugin.zsh
@@ -11,3 +11,18 @@ newhtml () {
 "\n    </body>"\
 "\n</html>" > $1 && $EDITOR $1
 }
+
+newphp () {
+    [[ -z $EDITOR ]] && EDITOR=vim
+    [[ -e $1 ]] && print "$1 exists.  Not modifying.\n" && $EDITOR $1
+    [[ ! -e $1 ]] && print "<?php"\
+"\n?>"\
+"\n<!DOCTYPE html>\n"\
+"\n<html>"\
+"\n    <head>"\
+"\n        <title></title>"\
+"\n    </head>"\
+"\n    <body>"\
+"\n    </body>"\
+"\n</html>" > $1 && $EDITOR $1
+}

d339593fffd9403c218ad633573c20ea4f30a215
ffomezolam <ffomezolam@gmail.com>
2013-05-01 18:35:34 -0400

    switched js to heredoc output

--- plugins/js/js.plugin.zsh
+++ plugins/js/js.plugin.zsh
@@ -3,44 +3,64 @@
 # create js with umd header
 newjsumd () {
     [[ -z $EDITOR ]] && EDITOR=vim
-    [[ -e $1 ]] && print "$1 exists.  Not modifying.\n" && $EDITOR $1
-    [[ ! -e $1 ]] && print "/**"\
-        "\n * @module"\
-        "\n */"\
-        "\n(function(name, context, definition) {"\
-        "\n    if(typeof module !== 'undefined' && module.exports) module.exports = definition(/*require(deps)*/);"\
-        "\n    else if(typeof define === 'function' && define.amd) define(/*[deps], */definition);"\
-        "\n    else context[name] = definition();"\
-        "\n})(/*'name'*/, this, function(/*deps*/) {"\
-        "\n"\
-        "\n});" > $1 && $EDITOR $1
+    if [[ -e $1 ]] then
+        print "$1 exists.  Not modifying.\n" $EDITOR $1
+    else
+        cat > $1 <<END
+/**
+ * @module
+ */
+(function(name, context, definition) {
+    if(typeof module !== 'undefined' && module.exports) module.exports = definition(/*require(deps)*/);
+    else if(typeof define === 'function' && define.amd) define(/*[deps], */definition);
+    else context[name] = definition();
+})(/*'name'*/, this, function(/*deps*/) {
+
+});
+END
+        $EDITOR $1
+    fi
 }
 
 # create build.js for requirejs compiler
 newrjsbuild () {
     [[ -z $EDITOR ]] && EDITOR=vim
-    [[ -e "build.js" ]] && print "build.js exists.  Not modifying.\n" && $EDITOR "build.js"
-    [[ ! -e "build.js" ]] && print "({"\
-        "\n    baseUrl: '.',"\
-        "\n    name: 'main',"\
-        "\n    out: 'main.build.js'"\
-        "\n})" > "build.js" && $EDITOR "build.js"
+    if [[ -e "build.js" ]] then
+        print "build.js exists.  Not modifying.\n"
+        $EDITOR "build.js"
+    else
+        cat > build.js <<END
+({
+    baseUrl: '.',
+    name: 'main',
+    out: 'main.build.js'
+})
+END
+        $EDITOR "build.js"
+    fi
 }
 
 # create yuidoc.json for yuidoc
 newyuidoc () {
     [[ -z $EDITOR ]] && EDITOR=vim
-    [[ -e "yuidoc.json" ]] && print "yuidoc.json exists.  Not modifying.\n" && $EDITOR "yuidoc.json"
-    [[ ! -e "yuidoc.json" ]] && print "{"\
-        "\n\t"'"name": "",'\
-        "\n\t"'"description": "",'\
-        "\n\t"'"version": "",'\
-        "\n\t"'"url": "",'\
-        "\n\t"'"options": {'\
-        "\n\t\t"'"outdir": "./doc",'\
-        "\n\t\t"'"exclude": "doc",'\
-        "\n\t\t"'"tabtospace": "4",'\
-        "\n\t\t"'"linkNatives": "true"'\
-        "\n\t"'}'\
-        "\n"'}' > "yuidoc.json" && $EDITOR "yuidoc.json"
+    if [[ -e "yuidoc.json" ]] then
+        print "yuidoc.json exists.  Not modifying.\n"
+        $EDITOR "yuidoc.json"
+    else
+        cat > yuidoc.json <<END
+{
+    "name": "",
+    "description": "",
+    "version": "",
+    "url": "",
+    "options": {
+        "outdir": "./doc",
+        "exclude": "doc",
+        "tabtospace": "4",
+        "linkNatives": "true"
+    }
+}
+END
+        $EDITOR "yuidoc.json"
+    fi
 }

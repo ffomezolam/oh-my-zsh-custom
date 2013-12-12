a2b3ee4a3bd83f6514115a236e1767c72c63264d
ffomezolam <ffomezolam@gmail.com>
2013-12-12 10:20:43 -0500

    updated html template script

--- plugins/html/html.plugin.zsh
+++ plugins/html/html.plugin.zsh
@@ -8,14 +8,15 @@ newhtml () {
         cat > $1 <<END
 <!DOCTYPE html>
 
-<!--[if lt IE 7]><html class="ie6"><![endif]-->
-<!--[if IE 7]><html class="ie7"><![endif]-->
-<!--[if IE 8]><html class="ie7"><![endif]-->
-<!--[if gt IE 8]><!--><html><!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
     </body>

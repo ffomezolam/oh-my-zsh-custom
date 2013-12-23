28c6535e5beeb4cd79a25e2c0cbe0617c9631506
ffomezolam <ffomezolam@gmail.com>
2013-12-23 15:16:08 -0500

    fixed missing end of meta tag

--- plugins/html/html.plugin.zsh
+++ plugins/html/html.plugin.zsh
@@ -14,7 +14,7 @@ newhtml () {
 <!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
     <head>
         <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
